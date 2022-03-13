2bfae3381016f31bb313226635f26f2e1eb9dd40
k-fog <59814110+k-fog@users.noreply.github.com>
2022-03-13 14:45:30 +0900

    feat: 🎸 トークナイザを追加

--- kcc.c
+++ kcc.c
@@ -1,33 +1,123 @@
+#include <ctype.h>
+#include <stdarg.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+// トークンの種類
+typedef enum {
+    TK_RESERVED, // 記号
+    TK_NUM,      // 整数
+    TK_EOF,      // 入力の終わり
+} TokenKind;
+
+typedef struct Token Token;
+
+// トークン型
+struct Token {
+    TokenKind kind; // トークンの種類
+    Token *next;    // 次のトークン
+    int val;        // TK_NUMの場合、その数値
+    char *str;      // トークン文字列
+};
+
+// 現在着目しているトークン
+Token *token;
+
+void error(char *fmt, ...) {
+    va_list ap;
+    va_start(ap, fmt);
+    vfprintf(stderr, fmt, ap);
+    fprintf(stderr, "\n");
+    exit(1);
+}
+
+// 次のトークンが期待した記号の場合、トークンを1つ読み進めて真を返す
+// それ以外の場合、偽を返す
+bool consume(char op) {
+    if(token->kind != TK_RESERVED || token->str[0] != op) return false;
+    token = token->next;
+    return true;
+}
+
+// 次のトークンが期待した記号の場合、トークンを1つ読み進める
+// それ以外の場合、エラー
+void expect(char op) {
+    if(token->kind != TK_RESERVED || token->str[0] != op) {
+        error("it's not '%c'", op);
+    }
+    token = token->next;
+}
+
+// 次のトークンが数値の場合、トークンを1つ読み進めてその数値を返す
+// それ以外の場合、エラー
+int expect_number() {
+    if(token->kind != TK_NUM) error("it's not a number.");
+    int val = token->val;
+    token = token->next;
+    return val;
+}
+
+bool at_eof() {
+    return token->kind == TK_EOF;
+}
+
+// 新しいトークンを作成し、curに繋げる
+Token *new_token(TokenKind kind, Token *cur, char *str) {
+    Token *t = calloc(1, sizeof(Token));
+    t->kind = kind;
+    t->str = str;
+    cur->next = t;
+    return t;
+}
+
+Token *tokenize(char *p) {
+    Token head;
+    head.next = NULL;
+    Token *cur = &head;
+
+    while(*p) {
+        if(isspace(*p)) {
+            p++;
+            continue;
+        }
+        if (*p == '+' || *p == '-') {
+            cur = new_token(TK_RESERVED, cur, p++);
+            continue;
+        }
+        if(isdigit(*p)) {
+            cur = new_token(TK_NUM, cur, p);
+            cur->val = strtol(p, &p, 10);
+            continue;
+        }
+        error("it can't be tokenized.");
+    }
+
+    new_token(TK_EOF, cur, p);
+    return head.next;
+}
 
 int main(int argc, char **argv) {
     if(argc != 2) {
-        fprintf(stderr, "the number of argments is not correct.");
+        error("the number of argments is not correct.");
         return 1;
     }
 
-    char *p = argv[1];
-    
+    token = tokenize(argv[1]);
+
     printf(".intel_syntax noprefix\n");
     printf(".globl main\n");
     printf("main:\n");
-    printf("  mov rax, %ld\n", strtol(p, &p, 10));
+    printf("  mov rax, %d\n", expect_number());
 
-    while(*p) {
-        if(*p == '+') {
-            p++;
-            printf("  add rax, %ld\n", strtol(p, &p, 10));
+    while(!at_eof()) {
+        if(consume('+')) {
+            printf("  add rax, %d\n", expect_number());
             continue;
         }
-        if(*p == '-') {
-            p++;
-            printf("  sub rax, %ld\n", strtol(p, &p, 10));
-            continue;
-        }
-
-        fprintf(stderr, "unexpected token: '%c'\n", *p);
-        return 1;
+        expect('-');
+        printf("  sub rax, %d\n", expect_number());
     }
 
     printf("  ret\n");
